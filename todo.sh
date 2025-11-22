3061f7a1599f0c80066e56016792209e04cb4e34
Love Asoh <loveasoh@gmail.com>
2025-11-22 10:03:33 +0100

    Implement color-coded output

--- todo.sh
+++ todo.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 FILE="tasks.txt"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
 
 to_clear(){
     clear
@@ -9,7 +11,7 @@ to_clear(){
 
     #This checks to confirm that an argument is provided
     if [[ -z "$1" ]]; then
-        echo "Wrong input, Usage : $0 <argument> <argument>"
+        echo -e "${RED}Wrong input, Usage : $0 <argument> <argument>"
         exit 1
     fi
     # Runs when the first argument($1) is provided and is correct
@@ -17,9 +19,9 @@ case "$1" in
     "add")
         if ! grep -q "^$2" "$FILE"; then
             echo "$2", "$(date) ">> $FILE
-            echo "You just added another task"
+            echo -e "${GREEN}You just added another task"
         else 
-        echo "This task already exist."
+        echo -e "${RED}This task already exist."
         fi
     ;;
     "list")
@@ -28,9 +30,9 @@ case "$1" in
     "del")
         if [[ $(wc -l  < $FILE) -ge 2 ]]; then 
             sed -i '2d' $FILE
-            echo "The second line of $FILE has been successfully deleted!"
+            echo -e "${GREEN}The second line of $FILE has been successfully deleted!"
         else 
-            echo "The $FILE contains only one line."
+            echo -e "${RED}The $FILE contains only one line."
         fi
     ;;
     "clear")
@@ -42,7 +44,7 @@ case "$1" in
         echo "The default input format is: $0 {add Shopping|list|del 2}"
     ;;
     *)
-        echo "Wrong format of arguments. Use $0 -h to see guides on the usage. "
+        echo -e "${RED}Wrong format of arguments. Use $0 -h to see guides on the usage. "
         exit 1
         ;;
 
