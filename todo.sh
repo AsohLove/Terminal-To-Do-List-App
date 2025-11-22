4a21cf05d440a10e4698e79db2b84122ee2ba68a
Love Asoh <loveasoh@gmail.com>
2025-11-22 02:11:15 +0100

    Add error message to task deletion

--- todo.sh
+++ todo.sh
@@ -2,23 +2,12 @@
 
 FILE="tasks.txt"
 
-
-#    if [[ $1 -eq "add" ]]; then
-#         echo "$2" >> tasks.txt
-#         break
-#     elif [[ $1 = "list" ]]; then
-#      cat n tasks.txt
-#     elif [[ $1 = "del" ]]; then 
-#         sed '2d' tasks.txt
-#     fi 
-# done
-
-# while [[ "$#" -gt 0 ]]; do
-
+    #This checks to confirm that an argument is provided
     if [[ -z "$1" ]]; then
         echo "Wrong input, Usage : $0 <argument> <argument>"
         exit 1
     fi
+    # Runs when the first argument($1) is provided and is correct
 case "$1" in 
     "add")
     echo "$2" >>$FILE
@@ -27,16 +16,23 @@ case "$1" in
     cat -n tasks.txt
     ;;
     "del")
-    sed -i '2d' $FILE
+        if [[ $(wc -l  < $FILE) -ge 2 ]]; then 
+            sed -i '2d' $FILE
+            echo "The second line of $FILE has been successfully deleted!"
+        else 
+            echo "The $FILE contains only one line."
+        fi
+
+    
     ;;
     "-h")
-        echo "You are required to input two arguments after todo.sh($0)"
-        echo "Default: $0 {add "Shopping"|list|del 2}"
+        echo "You are required to input two arguments after ($0)"
+        echo "  ============== " 
+        echo "The default way is as follows: $0 {add "Shopping"|list|del 2}"
     ;;
     *)
-        echo "Wrong arguments for $1"
+        echo "Wrong arguments for "
         exit 1
         ;;
 
 esac
-# done
