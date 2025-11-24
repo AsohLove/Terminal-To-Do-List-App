6d335ef1008b3e4a3ed280ed41a417a7371975e5
Love Asoh <loveasoh@gmail.com>
2025-11-24 09:03:24 +0100

    Assign argument values to variables

--- todo.sh
+++ todo.sh
@@ -4,33 +4,38 @@ FILE="tasks.txt"
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 
+arg1=$1
+arg2=$2
+
 to_clear(){
     clear
     echo "Terminal cleared"
 }
 
     #This checks to confirm that an argument is provided
-    if [[ -z "$1" ]]; then
+    if [[ -z "$arg1" ]]; then
         echo -e "${RED}Wrong input, Usage : $0 <argument> <argument>"
         exit 1
     fi
     # Runs when the first argument($1) is provided and is correct
-case "$1" in 
+case "$arg1" in 
     "add")
-        if ! grep -q "^$2" "$FILE"; then
-            echo "$2", "$(date) ">> $FILE
+        if ! grep -q "^$arg2" "$FILE"; then
+            echo "$arg2", $(date "+%Y-%m-%d %H:%M:%S") >> $FILE
             echo -e "${GREEN}You just added another task"
         else 
         echo -e "${RED}This task already exist."
         fi
     ;;
     "list")
-    cat -n tasks.txt
+    cat -n $FILE
     ;;
     "del")
-        if [[ $(wc -l  < $FILE) -ge 2 ]]; then 
+        if [[ $(wc -l  < $FILE) -ge 2 && $arg2 -eq 2 ]]; then 
             sed -i '2d' $FILE
-            echo -e "${GREEN}The second line of $FILE has been successfully deleted!"
+            echo -e "${GREEN}The second task of $FILE has been successfully deleted!"
+        elif [[ $arg2 -ne 2 ]]; then
+            echo "Your second argument should be 2"
         else 
             echo -e "${RED}The $FILE contains only one line."
         fi
